fc273e6b7de9a3735e1ec6d39d99fe3ec2b59f55
Natt Korat <135023751+nattkorat@users.noreply.github.com>
2024-08-13 00:48:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const quadratic = (a, b, c) => {
-    // complete the code here
+    let discriminant =  b*b - 4 * a * c
+    if (discriminant >= 0){
+        let x1 = (-b + Math.sqrt(discriminant)) / 2 * a
+        let x2 = (-b - Math.sqrt(discriminant)) / 2 * a
+        return `x1 = ${x1}, x2 = ${x2}`
+
+    }else{
+        return "No root!"
+    }
 }
 
 const formatSum = n => {
-    // complete the code here
+    if (n <= 1) return '1'
+    return formatSum(n - 1) + "+" + String(n)
 }
 
 
